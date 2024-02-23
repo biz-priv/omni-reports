dc2d64d16c0e4a487ccc5b851d4fcb0a764a4f7f
Jayanth <jayanth.vishwas@bizcloudexperts.com>
2024-02-23 12:11:01 -0600

    remove hard coded variables

--- src/danaherMonthlyReport/index.js
+++ src/danaherMonthlyReport/index.js
@@ -55,8 +55,7 @@ async function fetchDataFromRedshift(danaherMonthlyReportQuery) {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE
-    database: 'prod_datamodel'
+    database: process.env.DB_DATABASE
   });
   try {
     await client.connect();
