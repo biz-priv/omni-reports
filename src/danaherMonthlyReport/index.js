f12f9ba162b8944288429266831dd50ff101d7b1
Jayanth <jayanth.vishwas@bizcloudexperts.com>
2024-02-26 13:30:05 -0600

    testing

--- src/danaherMonthlyReport/index.js
+++ src/danaherMonthlyReport/index.js
@@ -55,7 +55,8 @@ async function fetchDataFromRedshift(danaherMonthlyReportQuery) {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    // database: process.env.DB_DATABASE
+    database: 'prod_datamodel'
   });
   try {
     const query = danaherMonthlyReportQuery
