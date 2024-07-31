76f328e6fb9db57f3ee0958c2e91b9edcb5e64dc
naveen03252 <naveen.chitturi@bizcloudexperts.com>
2024-07-31 19:39:29 +0530

    installing stream

--- src/doterraProject44Report/index.js
+++ src/doterraProject44Report/index.js
@@ -1,76 +1,103 @@
-// 'use strict'
+'use strict';
 
 const AWS = require('aws-sdk');
-// const fs = require('fs');
 const nodemailer = require('nodemailer');
-// Create SES transporter
+const { get } = require('lodash');
+const csv = require('csv-parser');
+const XLSX = require('xlsx');
+const stream = require('stream');
+
+const s3 = new AWS.S3();
 const ses = new AWS.SES({ apiVersion: '2010-12-01' });
+
 const transporter = nodemailer.createTransport({
   SES: ses
 });
-const { get } = require('lodash');
-const s3 = new AWS.S3();
 
-// Define email parameters
 const sender = 'no-reply@omnilogistics.com';
 const recipient = 'sreddy@bizcloudexperts.com, mohammed.sazeed@bizcloudexperts.com, sunilkunapareddys@gmail.com, mohammedsazeed2@gmail.com';
 const subject = 'doTerra Reports - 3107';
 const bodyText = 'Hello,\n\nPlease see the attached file.\n\nBest regards.';
-const attachmentFilePath = '/Users/mohammedsazeed/Downloads/doterra-2.xlsx';
-
 
 module.exports.handler = async (event, context) => {
   console.info(JSON.stringify(event));
   try {
-      const s3Bucket = get(event, 'Records[0].s3.bucket.name', '');
-      const s3Key = get(event, 'Records[0].s3.object.key', '');
-      console.log("s3Key",s3Key);
-      const s3Data = await getS3Data(s3Bucket, s3Key);
+    const s3Bucket = get(event, 'Records[0].s3.bucket.name', '');
+    const s3Key = get(event, 'Records[0].s3.object.key', '');
 
-      console.log("s3Data",s3Data);
+    const csvData = await getS3Data(s3Bucket, s3Key);
+    const processedData = await processCSV(csvData);
+
+    const attachmentBuffer = createExcelBuffer(processedData);
+    await sendEmail(attachmentBuffer);
 
     return 'Success';
   } catch (error) {
     console.error('Main lambda error: ', error);
-    // await putItem(dynamoData);
     return 'Failed';
   }
 };
 
 async function getS3Data(bucket, key) {
-  let params;
+  const params = { Bucket: bucket, Key: key };
   try {
-    params = { Bucket: bucket, Key: key };
     const response = await s3.getObject(params).promise();
     return response.Body.toString();
   } catch (error) {
-    console.error('Read s3 data: ', error, '\ns3 params: ', params);
+    console.error('Read S3 data: ', error, '\nS3 params: ', params);
     throw error;
   }
 }
-// Read the attachment
-// const attachment = fs.readFileSync(attachmentFilePath);
-console.info('🚀 ~ file: test.js:23 ~ attachment:', attachment)
 
-// Create the email options
-const mailOptions = {
-  from: sender,
-  to: recipient,
-  subject,
-  text: bodyText,
-  attachments: [
-    {
-      filename: 'doTerra_reports_3107.xlsx',
-      content: attachment
+function processCSV(csvData) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(Buffer.from(csvData));
+
+    bufferStream
+      .pipe(csv({ headers: false }))
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        const processedData = results.map(row => ({
+          shipment_id: row[0] || 'NULL',
+          bol: row[1] || 'NULL',
+          carrier_id: row[2] || 'NULL',
+          container_id: row[3] || 'NULL',
+          order_ref: row[4] || 'NULL',
+        }));
+        resolve(processedData);
+      })
+      .on('error', (error) => reject(error));
+  });
+}
+
+function createExcelBuffer(data) {
+  const wb = XLSX.utils.book_new();
+  const ws = XLSX.utils.json_to_sheet(data);
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  return XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' });
+}
+
+async function sendEmail(attachmentBuffer) {
+  const mailOptions = {
+    from: sender,
+    to: recipient,
+    subject,
+    text: bodyText,
+    attachments: [
+      {
+        filename: 'doTerra_reports_3107.xlsx',
+        content: attachmentBuffer
+      }
+    ]
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.error('Error sending email:', err);
+    } else {
+      console.info('Email sent! Message ID:', info.messageId);
     }
-  ]
-};
-
-// Send the email
-transporter.sendMail(mailOptions, (err, info) => {
-  if (err) {
-    console.error('Error sending email:', err);
-  } else {
-    console.info('Email sent! Message ID:', info.messageId);
-  }
-});
+  });
+}
